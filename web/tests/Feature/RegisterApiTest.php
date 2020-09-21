df447b4d6400ded949d2aec5311657f064c5846f
金谷翔平 <kanatani1113@gmail.com>
2020-09-21 19:04:47 +0900

    登録処理の雛形作成

---
+++ web/tests/Feature/RegisterApiTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class RegisterApiTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * @test
+     */
+    public function should_新しいユーザーを作成して返却する()
+    {
+        $data = [
+            'name' => 'skanatani',
+            'email' => 'kanatani1113@gmail.com',
+            'password' => 'shohei1113',
+            'password_confirmation' => 'shohei1113',
+        ];
+
+        $response = $this->json('POST', route('register'), $data);
+
+        $user = User::first();
+        $this->assertEquals($data['name'], $user->name);
+
+        $response
+            ->assertStatus(201)
+            ->assertJson(['name' => $user->name]);
+    }
+}
